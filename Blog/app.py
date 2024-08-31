139a9cff900c5179b14a22e1fb967da27e092fda
ashimdhungana07 <ashimdhungana6@gmail.com>
2024-08-31 20:32:37 +0545

    new

--- Blog/app.py
+++ Blog/app.py
@@ -47,6 +47,7 @@ class Posts(db.Model):
     title = db.Column(db.String(30), nullable=False)
     slug = db.Column(db.String(25), nullable=False)
     content = db.Column(db.String(500), nullable=False)
+    img_file = db.Column(db.String(12), nullable=False)
 
 
 @app.route("/")
@@ -81,15 +82,15 @@ def contact():
     return render_template('contact.html')
 
 
-# @app.route("/post/<string:post_slug>", methods=['GET'])
-# def post(post_slug):
-#     post = Posts.query.filter_by(slug=post_slug).first()
+@app.route("/post/<string:post_slug>", methods=['GET'])
+def post_route(post_slug):
+    post = Posts.query.filter_by(slug=post_slug).first()
 
+    return render_template('post.html', params=params, post=post)
+
+# @app.route("/post")
+# def post():
 #     return render_template('post.html')
-
-@app.route("/post")
-def post():
-    return render_template('post.html')
 
 
 app.run(debug=True)
